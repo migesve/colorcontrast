cd08780d81935317e3a89d4c6bb1ac19bb310e4a
Miges <miguelangelespinal@gmail.com>
2022-11-18 21:36:16 +0100

    boooooom

--- exp.js
+++ exp.js
@@ -1,136 +1,161 @@
-console.log('=========================')
+console.log("=========================");
 
-let mots = ['Tableau', 'Maladie','Acceder','Haricot','Tension','Pendule','Parasol','Unanime','Courant','Autiste'];
-let jaune = '#dddd00';
-let bleu = '#0000ff';
+let mots = [
+  "Tableau",
+  "Maladie",
+  "Acceder",
+  "Haricot",
+  "Tension",
+  "Pendule",
+  "Parasol",
+  "Unanime",
+  "Courant",
+  "Autiste",
+];
+let motATrouver;
+let jaune = "#dddd00";
+let bleu = "#0000ff";
 
-let grisC = '#888888'; // 2.82 avec bleu, 2.42 avec bleu2 et 2.43 avec jaune
-let grisF = '#b1b1b1'; // 1.71 avec bleu, 4 avec bleu2 et 1.47 avec jaune
-let gris3 = ''; // bfbfbf?
+let grisC = "#888888"; // 2.82 avec bleu, 2.42 avec bleu2 et 2.43 avec jaune
+let grisF = "#b1b1b1"; // 1.71 avec bleu, 4 avec bleu2 et 1.47 avec jaune
+let gris3 = ""; // bfbfbf?
 //let grisCC = '#333333';
-let couleurs = [jaune,bleu,grisC,grisF];
+let couleurs = [jaune, bleu, grisC, grisF];
 NewMots = arrayShuffle(mots);
-CoulMots = [couleurs[0],couleurs[1]];
-CoulFond = [couleurs[2],couleurs[3]];
-tabrep = ['bt gc-j','bt gc-b','bt gf-j','bt gf-b','bt gc-j','bt gc-b','bt gf-j','bt gf-b','bt gc-j','bt gc-b','bt gf-j','bt gf-b','bt gc-j','bt gc-b','bt gf-j','bt gf-b','bt gc-j','bt gc-b','bt gf-j','bt gf-b','bt gc-j','bt gc-b','bt gf-j','bt gf-b','bt gc-j','bt gc-b','bt gf-j','bt gf-b','bt gc-j','bt gc-b','bt gf-j','bt gf-b','bt gc-j','bt gc-b','bt gf-j','bt gf-b','bt gc-j','bt gc-b','bt gf-j','bt gf-b'];
+CoulMots = [couleurs[0], couleurs[1]];
+CoulFond = [couleurs[2], couleurs[3]];
+tabrep = [
+  "bt gc-j",
+  "bt gc-b",
+  "bt gf-j",
+  "bt gf-b",
+  "bt gc-j",
+  "bt gc-b",
+  "bt gf-j",
+  "bt gf-b",
+  "bt gc-j",
+  "bt gc-b",
+  "bt gf-j",
+  "bt gf-b",
+  "bt gc-j",
+  "bt gc-b",
+  "bt gf-j",
+  "bt gf-b",
+  "bt gc-j",
+  "bt gc-b",
+  "bt gf-j",
+  "bt gf-b",
+  "bt gc-j",
+  "bt gc-b",
+  "bt gf-j",
+  "bt gf-b",
+  "bt gc-j",
+  "bt gc-b",
+  "bt gf-j",
+  "bt gf-b",
+  "bt gc-j",
+  "bt gc-b",
+  "bt gf-j",
+  "bt gf-b",
+  "bt gc-j",
+  "bt gc-b",
+  "bt gf-j",
+  "bt gf-b",
+  "bt gc-j",
+  "bt gc-b",
+  "bt gf-j",
+  "bt gf-b",
+];
 //document.write(tabrep.length)
 tabrep = arrayShuffle(tabrep);
 let counterMots = 0;
-let headdata = ["Mot a trouver","index","Mot cliqué","date",,"classe"]; //dans quel ordre son les données : si index = 12 btn Go sinon mot rechercher
-let data =[];
+let headdata = ["Mot a trouver", "index", "Mot cliqué", "date", , "classe"]; //dans quel ordre son les données : si index = 12 btn Go sinon mot rechercher
+let data = [];
 
 function arrayShuffle(a) {
-    var l = a.length, t, r;
-    while (0 !== l) {
-        r = Math.floor(Math.random() * l);
-        l -= 1;
-        t = a[l];
-        a[l] = a[r];
-        a[r] = t;
-    }
-    return a;
+  var l = a.length,
+    t,
+    r;
+  while (0 !== l) {
+    r = Math.floor(Math.random() * l);
+    l -= 1;
+    t = a[l];
+    a[l] = a[r];
+    a[r] = t;
+  }
+  return a;
 }
 
-function entierAleatoire(min, max){
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+function entierAleatoire(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function clickListener(e) {  
-    
+function clickListener(e) {
+  //console.log(counterMots);
+  var clickedElement = window.event ? window.event.target : e.target,
+    tags = document.getElementsByTagName(clickedElement.tagName);
 
-    //console.log(counterMots);
-    var clickedElement=(window.event)
-        ? window.event.target
-        : e.target,
-        tags = document.getElementsByTagName (clickedElement.tagName);
-        
-        console.log({motATrouver:document.getElementById("motATrouver").textContent,id:clickedElement.id,MotClicke:clickedElement.value,style:clickedElement.className})
-/*
-    for (var i = 0; i < tags.length; ++i) {
-        if (tags [i] == clickedElement) {
-            //arrayWithElements.push ({tag:clickedElement.tagName,index:i}); 
-
-
-            // on a pas besoin let index = document.querySelectorAll('input[value="' + document.getElementById("motATrouver").textContent + '"]');
-            //console.log(tags)
-            //let time = Date.now();
-           
-            //savedata ({tag:clickedElement.tagName,index:i,time});
-          
-        }
+  NewMots = arrayShuffle(mots);
+  //if (counterMots% 2 == 0){
+  if (clickedElement.value == "Go") {
+    motATrouver = NewMots[entierAleatoire(0, 9)];
+    for (let i = 0; i < 10; i++) {
+      document.getElementById(i).value = NewMots[i];
+      document.getElementById(i).className = tabrep[counterMots];
+      document.getElementById(i).style.visibility = "visible";
     }
-*/
-
-    NewMots = arrayShuffle(mots);
-    document.getElementById(11).value = Math.trunc((counterMots+1)/2)+"/40";
-    if (counterMots% 2 == 0){
-        for(let i = 0; i < 10; i++){
-            //document.getElementById(i).style.backgroundColor = CoulFond[entierAleatoire(0,1)];
-            //document.getElementById(i).style.color = CoulMots[entierAleatoire(0,1)];
-            document.getElementById(i).value = NewMots[i];
-            document.getElementById("motATrouver").textContent = NewMots[entierAleatoire(0,9)];
-            document.getElementById(i).className = tabrep[(counterMots/2)+1];
-            
-        }
+    document.getElementById("motATrouver").textContent = motATrouver;
+    document.getElementById("motATrouver").style.visibility = "visible";
+    document.getElementById("texte1").textContent = "Trouvez le mot:";
+    document.getElementById(12).disabled = true;
+  } else {
+    //document.getElementById(i).style.backgroundColor = CoulFond[entierAleatoire(0,1)];
+    //document.getElementById(i).style.color = CoulMots[entierAleatoire(0,1)];
+    document.getElementById("motATrouver").style.visibility = "hidden";
+    document.getElementById("texte1").textContent =
+      "Clickez sur le button XXXXX";
+    document.getElementById(12).disabled = false;
+    for (let i = 0; i < 10; i++) {
+      document.getElementById(i).style.visibility = "hidden";
     }
-   
-    
+
+    // ce qu'on cherche garder dans le JSON
+    console.log({
+      essai: counterMots,
+      motATrouver: document.getElementById("motATrouver").textContent,
+      id: clickedElement.id,
+      MotClicke: clickedElement.value,
+      style: clickedElement.className,
+    });
+
     counterMots++;
-    if (counterMots >= 80){
-        document.write("<p class=''> L'expérience est fini, merci de votre participation  </p>")
-    }
-    
-    counterMots++;
+
+    document.getElementById(11).value = Math.trunc(counterMots) + "/40";
+  }
+
+  //}
+
+  if (counterMots >= 40) {
+    document.getElementById("texte1").textContent =
+      "L'expérience est fini, merci de votre participation";
+  }
 }
 
-function clickedId(clickedId){
-    console.log(clickedId);
-    
+function clickedId(clickedId) {
+  console.log(clickedId);
 }
 
-function savedata (data) {
+function savedata(data) {
+  // Creating a XHR object
+  let xhr = new XMLHttpRequest();
+  let url = "savedata.php";
 
-    // Creating a XHR object
-    let xhr = new XMLHttpRequest();
-    let url = "savedata.php";
+  // open a connection
+  xhr.open("POST", url, true);
 
-    // open a connection
-    xhr.open ("POST", url, true);
+  // Set the request header i.e. which type of content you are sending
+  xhr.setRequestHeader("Content-Type", "application/json");
 
-    // Set the request header i.e. which type of content you are sending
-    xhr.setRequestHeader ("Content-Type", "application/json");
-
-    // Sending data with the request
-    xhr.send (JSON.stringify (data));
+  // Sending data with the request
+  xhr.send(JSON.stringify(data));
 }
-
-
-/*
-NewMots = arrayShuffle(mots);
-document.getElementById(11).value = Math.trunc((counterMots+1))+"/40";
-//if (counterMots% 2 == 0){
-    if(clickedElement.value == "go"){
-        document.getElementById(i).value = NewMots[i];
-        document.getElementById("motATrouver").textContent = NewMots[entierAleatoire(0,9)];
-        console.log("hola");
-
-    }else{
-    for(let i = 0; i < 10; i++){
-        //document.getElementById(i).style.backgroundColor = CoulFond[entierAleatoire(0,1)];
-        //document.getElementById(i).style.color = CoulMots[entierAleatoire(0,1)];
-        document.getElementById(i).className = tabrep[(counterMots)+1];
-        document.getElementById("motATrouver").textContent = "hola";
-
-
-        counterMots++;
-        
-    }
-}
-//}
-
-
-if (counterMots >= 40){
-    document.write("<p class=''> L'expérience est fini, merci de votre participation  </p>")
-}
-}
-*/
