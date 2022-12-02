271a6e0a1d833a28a121e966e515b373d5b13163
Miges <miguelangelespinal@gmail.com>
2022-12-02 18:00:50 +0100

    chque modalite per bouton

--- exp.js
+++ exp.js
@@ -45,7 +45,7 @@ let mots = [
 
 let motATrouver;
 // listeMots = new Array();
-// tabrep = new Array();
+// newTabrep = new Array();
 //copier liste mots dans une nouvelle reference
 
 arrayShuffle(mots);
@@ -75,49 +75,70 @@ for (let i = 0; i < 9; i++) {
 
 //console.log(listeMots);
 tabrep = [
-  "bt gc-j",
-  "bt gc-b",
-  "bt gf-j",
-  "bt gf-b",
-  "bt gc-j",
-  "bt gc-b",
-  "bt gf-j",
-  "bt gf-b",
-  "bt gc-j",
-  "bt gc-b",
-  "bt gf-j",
-  "bt gf-b",
-  "bt gc-j",
-  "bt gc-b",
-  "bt gf-j",
-  "bt gf-b",
-  "bt gc-j",
-  "bt gc-b",
-  "bt gf-j",
-  "bt gf-b",
-  "bt gc-j",
-  "bt gc-b",
-  "bt gf-j",
-  "bt gf-b",
-  "bt gc-j",
-  "bt gc-b",
-  "bt gf-j",
-  "bt gf-b",
-  "bt gc-j",
-  "bt gc-b",
-  "bt gf-j",
-  "bt gf-b",
-  "bt gc-j",
-  "bt gc-b",
-  "bt gf-j",
-  "bt gf-b",
-  "bt gc-j",
-  "bt gc-b",
-  "bt gf-j",
-  "bt gf-b",
+  ["gc-j", "gc-b", "gf-j", "gf-b"],
+  ["gc-j", "gc-b", "gf-j", "gf-b"],
+  ["gc-j", "gc-b", "gf-j", "gf-b"],
+  ["gc-j", "gc-b", "gf-j", "gf-b"],
+  ["gc-j", "gc-b", "gf-j", "gf-b"],
+  ["gc-j", "gc-b", "gf-j", "gf-b"],
+  ["gc-j", "gc-b", "gf-j", "gf-b"],
+  ["gc-j", "gc-b", "gf-j", "gf-b"],
+  ["gc-j", "gc-b", "gf-j", "gf-b"],
+  ["gc-j", "gc-b", "gf-j", "gf-b"],
 ];
-//document.write(tabrep.length)
-arrayShuffle(tabrep);
+let newTabrep = new Array();
+for (let i = 0; i < 10; i++) {
+  for (let j = 0; j < 4; j++) {
+    newTabrep.push(tabrep[i][j] + "." + i);
+  }
+}
+
+console.log(newTabrep);
+newTabrep = [
+  "gc-j.0",
+  "gc-b.0",
+  "gf-j.0",
+  "gf-b.0",
+  "gc-j.1",
+  "gc-b.1",
+  "gf-j.1",
+  "gf-b.1",
+  "gc-j.2",
+  "gc-b.2",
+  "gf-j.2",
+  "gf-b.2",
+  "gc-j.3",
+  "gc-b.3",
+  "gf-j.3",
+  "gf-b.3",
+  "gc-j.4",
+  "gc-b.4",
+  "gf-j.4",
+  "gf-b.4",
+  "gc-j.5",
+  "gc-b.5",
+  "gf-j.5",
+  "gf-b.5",
+  "gc-j.6",
+  "gc-b.6",
+  "gf-j.6",
+  "gf-b.6",
+  "gc-j.7",
+  "gc-b.7",
+  "gf-j.7",
+  "gf-b.7",
+  "gc-j.8",
+  "gc-b.8",
+  "gf-j.8",
+  "gf-b.8",
+  "gc-j.9",
+  "gc-b.9",
+  "gf-j.9",
+  "gf-b.9",
+];
+
+//document.write(newTabrep.length)
+arrayShuffle(newTabrep);
 let counterMots = 0;
 let headdata = ["Mot a trouver", "index", "Mot cliqué", "date", , "classe"]; //dans quel ordre son les données : si index = 12 btn Go sinon mot rechercher
 let data = [];
@@ -158,8 +179,7 @@ function clickListener(e) {
     // container.appendChild(divContent);
     // container.appendChild(divIntro);
 
-    var age =
-      (2022 - document.getElementById("naiss").value);
+    var age = 2022 - document.getElementById("naiss").value;
     if (age < 18) {
       if (document.getElementById("plus18")) {
         break id1;
@@ -172,26 +192,27 @@ function clickListener(e) {
 
       g.id = "plus18";
 
-      document.getElementById("naiss")
+      document
+        .getElementById("naiss")
         .parentNode.insertBefore(g, document.getElementById("naiss"));
 
       document.getElementById("plus18").style.color = "red";
       break id1;
-    } 
-    console.log(age)
+    }
+    console.log(age);
     // else if (age >= 18) {
     //   document.getElementById("plus18").style.visibility = "hidden";
     // }
 
-    if ((document.getElementById("checkbox").checked == false)
-    || (document.getElementById("naiss").textContent == null) ||
-    (!document.getElementById("Sexe-0").checked &&
-        !document.getElementById("Sexe-1").checked&&
-        !document.getElementById("Sexe-2").checked)) {
+    if (
+      document.getElementById("checkbox").checked == false &&
+      document.getElementById("naiss").textContent == "" &&
+      document.getElementById("Sexe").textContent == ""
+    ) {
       document.getElementsByClassName(
         "formbuilder-checkbox-group-label"
       )[0].textContent =
-        "S'il vous plaît remplisser toutes les informations et cocher cette case pour accepter de participer à notre expérience.";
+        "S'il vous plaît cocher cette case pour accepter de participer à notre expérience.";
       document.getElementsByClassName(
         "formbuilder-checkbox-group-label"
       )[0].style.color = "red";
@@ -224,36 +245,64 @@ function clickListener(e) {
     start = Date.now();
     //console.log(dixMots, dixMots[0]);
     dixMots = arrayShuffle(dixMots);
-    //console.log(dixMots);
-    let latence = entierAleatoire(500,1300)
-    setTimeout(() => {  
-    
-    for (let i = 0; i < 10; i++) {
-      document.getElementById(i).value = dixMots[i];
-      //console.log(dixMots[i]);
-      document.getElementById(i).className = tabrep[counterMots];
-      document.getElementById(i).style.visibility = "visible";
-    }
- 
-    document.getElementById("motATrouver").textContent = motATrouver;
-    document.getElementById("motATrouver").style.visibility = "visible";
-    document.getElementById("texte1").textContent = "Trouvez le mot:";
-    document.getElementById(12).disabled = true;
+    //splits modalite et lieu de mot a trouver
+    let str = String(newTabrep[counterMots]);
+    let splitTabreb = str.split(".");
 
-    dixMots = new Array();
-    dixMots.push(listeMots[0]);
-    motATrouver = dixMots[0];
-    //console.log(listeMots, motATrouver, dixMots[0]);
-    listeMots.shift();
-    //console.log(listeMots, motATrouver, dixMots[0]);
-    for (let i = 0; i < 9; i++) {
-      //while pour ne pas repeter le meme mot
-      do {
-        aleatoire = entierAleatoire(0, mots.length - 1);
-      } while (dixMots.includes(mots[aleatoire]));
-      dixMots.push(mots[aleatoire]);
-    }
-  }, latence)
+    //console.log(dixMots);
+    let latence = entierAleatoire(300, 900);
+    //document.getElementById(i).value = splitTabreb[1];
+
+    document.getElementById(12).disabled = true;
+    setTimeout(() => {
+      for (let i = 0; i < 10; i++) {
+        if (i == splitTabreb[1] && dixMots[i] != motATrouver) {
+          if (dixMots.indexOf(motATrouver) - splitTabreb[1] < 0) {
+            document.getElementById(dixMots.indexOf(motATrouver)).value =
+              dixMots[i];
+            document.getElementById(i).value = motATrouver;
+
+            document.getElementById(i).className = splitTabreb[0];
+            document.getElementById(i).style.visibility = "visible";
+            
+            continue;
+          } /* */ else if (
+            dixMots.indexOf(motATrouver) - splitTabreb[1] - splitTabreb[1] >
+            0
+          ) {
+            document.getElementById(i).value = motATrouver;
+            dixMots[dixMots.indexOf(motATrouver)] = dixMots[i];
+
+            document.getElementById(i).className = splitTabreb[0];
+            document.getElementById(i).style.visibility = "visible";
+            
+            continue;
+          }
+        }
+        document.getElementById(i).value = dixMots[i];
+        //console.log(dixMots[i]);
+        document.getElementById(i).className = splitTabreb[0];
+        document.getElementById(i).style.visibility = "visible";
+
+      }
+
+      document.getElementById("motATrouver").textContent = motATrouver;
+      document.getElementById("motATrouver").style.visibility = "visible";
+      document.getElementById("texte1").textContent = "Trouvez le mot:";
+
+      dixMots = new Array();
+      dixMots.push(listeMots[0]);
+      motATrouver = dixMots[0];
+      listeMots.shift();
+      //console.log(listeMots, motATrouver, dixMots[0]);
+      for (let i = 0; i < 9; i++) {
+        //while pour ne pas repeter le meme mot
+        do {
+          aleatoire = entierAleatoire(0, mots.length - 1);
+        } while (dixMots.includes(mots[aleatoire]));
+        dixMots.push(mots[aleatoire]);
+      }
+    }, latence);
     //console.log(listeMots, motATrouver, dixMots[0]);
   } else {
     //document.getElementById(i).style.backgroundColor = CoulFond[entierAleatoire(0,1)];
@@ -273,10 +322,9 @@ function clickListener(e) {
       motATrouver: document.getElementById("motATrouver").textContent,
       id: clickedElement.id,
       MotClicke: clickedElement.value,
-      backgroundColor: document.getElementById(clickedElement.id)
-        .style.backgroundColor,
-      textColor: document.getElementById(clickedElement.id).style
-        .color,
+      backgroundColor: document.getElementById(clickedElement.id).style
+        .backgroundColor,
+      textColor: document.getElementById(clickedElement.id).style.color,
       timeElapsedSeconds: millis / 1000,
     });
 
@@ -284,10 +332,9 @@ function clickListener(e) {
       motATrouver: document.getElementById("motATrouver").textContent,
       id: clickedElement.id,
       motClicke: clickedElement.value,
-      backgroundColor: document.getElementById(clickedElement.id)
-        .style.backgroundColor,
-      textColor: document.getElementById(clickedElement.id).style
-        .color,
+      backgroundColor: document.getElementById(clickedElement.id).style
+        .backgroundColor,
+      textColor: document.getElementById(clickedElement.id).style.color,
       tempsDeReponse: millis / 1000,
     });
 
